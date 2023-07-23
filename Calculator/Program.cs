a0bb1a226a6e953e61fdc81739f4a50cbfe3ca99
Gabriel Concli <gabiconcli@gmail.com>
2023-07-23 15:07:11 -0300

    Corrigido opção voltar dentro do switchAdicionar arquivos de projeto.

---
+++ Calculator/Program.cs
@@ -0,0 +1,68 @@
+﻿double n1, n2, n3;
+string option;
+
+Console.WriteLine("Bem-vindo a calculadora");
+Console.WriteLine("\nEscolha uma opção: ");
+Console.WriteLine("1 - Somar");
+Console.WriteLine("2 - Subtrair");
+Console.WriteLine("3 - Multiplicar");
+Console.WriteLine("4 - Dividir");
+Console.WriteLine("0 - Sair");
+option = Console.ReadLine();
+
+switch (option)
+{
+    case "1":
+        Console.Clear();
+        Console.WriteLine("Digite o primeiro valor: ");
+        n1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite o segundo valor: ");
+        n2 = Convert.ToDouble(Console.ReadLine());
+
+        n3 = n1 + n2;
+        Console.WriteLine("Resultado: " + n3);
+        break;
+
+    case "2":
+        Console.Clear();
+        Console.WriteLine("Digite o primeiro valor: ");
+        n1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("\nDigite o segundo valor: ");
+        n2 = Convert.ToDouble(Console.ReadLine());
+
+        n3 = n1 - n2;
+        Console.WriteLine("\nResultado: " + n3);
+        break;
+
+    case "3":
+        Console.Clear();
+        Console.WriteLine("\nDigite o primeiro valor: ");
+        n1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("\nDigite o segundo valor: ");
+        n2 = Convert.ToDouble(Console.ReadLine());
+
+        n3 = n1 * n2;
+        Console.WriteLine("Resultado: " + n3);
+        break;
+
+    case "4":
+        Console.Clear();
+        Console.WriteLine("\nDigite o primeiro valor: ");
+        n1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("\nDigite o segundo valor: ");
+        n2 = Convert.ToDouble(Console.ReadLine());
+
+        n3 = n1 / n2;
+        Console.WriteLine("\nResultado: " + n3);
+        break;
+
+    case "0":
+        Console.Clear();
+        Console.WriteLine("Volte sempre!");
+        break;
+
+    default:
+        Console.Clear();
+        Console.WriteLine("Opção inválida.");
+        break;
+}
